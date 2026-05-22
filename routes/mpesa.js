ed018b04229871f90923b937e6f840732d19231a
Stacie100 <kibetstacie42@gmail.com>
2026-05-23 00:10:32 +0300

    fix: restore createClient import

--- routes/mpesa.js
+++ routes/mpesa.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js"; // 👈 this was deleted, add it back
 
 dotenv.config();
 
